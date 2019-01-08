fb995a50f0da7205f0db0e63b7906fc0db0dc683
Amara Cisse <amaracisse@amaras-mbp.home>
2019-01-08 12:56:55 -0500

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Amara Cisse on 1/8/19.
+//  Copyright © 2019 Amara Cisse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
